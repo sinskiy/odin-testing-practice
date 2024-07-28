db420c96dc18e1d102617865688edfe0793da350
sinskiy <sinskiy@proton.me>
2024-07-28 15:52:09 +0300

    feat: add reverseString() function

--- src/reverseString.test.js
+++ src/reverseString.test.js
@@ -1,1 +1,11 @@
-test("Jest works", () => expect(1 + 1).toBe(2));
+import reverseString from "./reverseString";
+
+test("Reverses empty string correctly", () => {
+  expect(reverseString("")).toBe("");
+});
+test("Reverses palindromes correctly", () => {
+  expect(reverseString("madam")).toBe("madam");
+});
+test("Reverses normal strings", () => {
+  expect(reverseString("abcdefg")).toBe("gfedcba");
+});
